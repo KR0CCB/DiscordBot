e4fa94e7edb46bf428b0505ecfff14592e83f507
Gyubeen <32409617+daegukgb@users.noreply.github.com>
2017-12-17 22:07:10 +0900

    Add files via upload

--- 0CCBBot/src/main/java/me/discordbots/Commands.java
+++ 0CCBBot/src/main/java/me/discordbots/Commands.java
@@ -3,21 +3,42 @@ package me.discordbots;
 import me.discordbots.commands.Ping;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-
+import me.discordbots.commands.Math;
 public class Commands extends ListenerAdapter{
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
-        String[] command = event.getMessage().getContent().split("");
+        String[] command = event.getMessage().getContent().split(" ");
         String message = event.getMessage().getContent();
-        System.out.println(command[0]);
+
         if(!command[0].startsWith("@")) return;
 
-
         if(Ping.isPingCommand(command)){
-            String sendMsg = "퐁!" + event.getJDA().getPing();
+
+            String sendMsg = "퐁! " + event.getJDA().getPing() + " /ms";
             event.getChannel().sendMessage(sendMsg).queue();
         }
+
+        if(command[0].equalsIgnoreCase("@계산기")){
+            if(command[1].equalsIgnoreCase("+")){
+                String sendMsg = String.valueOf(Math.Plus(Double.parseDouble(command[2]),Double.parseDouble(command[3])));
+                event.getChannel().sendMessage(sendMsg).queue();
+            }
+            else if(command[1].equalsIgnoreCase("-")){
+                String sendMsg = String.valueOf(Math.Minus(Double.parseDouble(command[2]),Double.parseDouble(command[3])));
+                event.getChannel().sendMessage(sendMsg).queue();
+            }
+            else if(command[1].equalsIgnoreCase("*")){
+                String sendMsg = String.valueOf(Math.Multiplication(Double.parseDouble(command[2]),Double.parseDouble(command[3])));
+                event.getChannel().sendMessage(sendMsg).queue();
+            }
+            else if(command[1].equalsIgnoreCase("/")){
+                String sendMsg = String.valueOf(Math.Division(Double.parseDouble(command[2]),Double.parseDouble(command[3])));
+                event.getChannel().sendMessage(sendMsg).queue();
+            }
+        }
+
+
 
     }
 }
